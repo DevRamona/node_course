4706a7f0e2528761408e056116eb95e2649f5d83
DevRamona <r.ingabire@alustudent.com>
2024-08-21 10:19:45 +0200

    An object that contains the imported variables

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const names = ["Yoha", "Gerg", "New"]
-const ages = ["10","20", "30", "40"]
-const isComplete = true
+
 module.exports = {
-    names, ages, isComplete
+    names :["Yoha", "Gerg", "New"] ,
+     ages : ["10","20", "30", "40"], 
+     isComplete : true
 }
