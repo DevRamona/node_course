9befa408a177bd4b26e98e1b477470a36586530b
DevRamona <r.ingabire@alustudent.com>
2024-08-21 15:10:29 +0200

    return html pages

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require("http")
+const fs = require("fs")
 const server = http.createServer((request, response) => {
     console.log(request.closed, request.url)
+    response.setHeader("Content-Type" , "text/html")
+    fs.readFile("./views/index.html", (err, data) => {
+        if(err) {
+            console.log(err)
+            response.end()
+        } else {
+            response.write(data)
+            response.end()
+        }
+    })
 })
+
+
+
 
 server.listen(3000, "localhost", () => {
     console.log("Listening for the requests on 3000 ")
