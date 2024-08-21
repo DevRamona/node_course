7319868d682e6634bca88b43e3d2ac35da18d5a1
DevRamona <r.ingabire@alustudent.com>
2024-08-21 15:37:46 +0200

    basic routing

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const http = require("http")
 const fs = require("fs")
 const server = http.createServer((request, response) => {
-    console.log(request.closed, request.url)
+
     response.setHeader("Content-Type" , "text/html")
-    fs.readFile("./views/index.html", (err, data) => {
+
+    let path = "./views/"
+    switch(request.url) {
+        case "/" :
+        path += "index.html";
+        response.statusCode = 200
+        break;
+        case "/about" :
+        path += "about.html";
+        response.statusCode = 200
+        case "/about-me" :
+        response.statusCode = 301
+        response.setHeader('Location', '/about')
+        break;
+        default :
+        path += "404.html";
+        response.statusCode = 301
+        break;
+    }
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err)
             response.end()
